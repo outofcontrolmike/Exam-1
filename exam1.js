5192b3cb6d020a889a0b3be15ce5e624613627ef
WILSON <mw0415436@otc.edu>
2018-09-17 17:15:00 -0500

    worked on this

---
+++ exam1.js
@@ -0,0 +1,28 @@
+// Michael Wilson
+// 09/18/18
+
+// Creating a shopping cart object
+
+const person = 
+
+//Object properties
+{
+    firstname: "Mike",
+    lastname: "Wilson"
+}
+
+//Object array
+const items = ["Pez", "Amethyst", "Wisdom"]
+
+
+//Display name
+function describe(person) {
+  return `${person.firstname} ${person.lastname}`;
+}
+console.log("name:"), 
+console.log(describe(person));
+
+for (i = 0; i < items.length; i++)
+    {
+        console.log(items[i]);
+    }
